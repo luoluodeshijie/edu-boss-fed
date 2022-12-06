26882979c35eeac5734403a11d50c3f0f0c499d7
limingxian <limingxianweb@163.com>
2022-12-06 22:12:15 +0800

    调整初始目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    11111
+    <!-- 根路由出口 -->
+    <router-view />
   </div>
 </template>
 
