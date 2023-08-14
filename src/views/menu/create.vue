8f35d94bb39f8302e12c82babdbf1b19bdb14083
lmx <lmx@163.com>
2023-08-14 17:54:55 +0800

    菜单管理-添加菜单-布局；数据绑定

---
+++ src/views/menu/create.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="menu-create">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>添加菜单</span>
+      </div>
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="菜单名称">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="菜单路径">
+          <el-input v-model="form.href"></el-input>
+        </el-form-item>
+        <el-form-item label="上级菜单">
+          <el-select v-model="form.parentId" placeholder="请选择上级菜单">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="描述">
+          <el-input v-model="form.description"></el-input>
+        </el-form-item>
+        <el-form-item label="前端图标">
+          <el-input v-model="form.icon"></el-input>
+        </el-form-item>
+        <el-form-item label="是否显示">
+          <el-radio-group v-model="form.shown">
+            <el-radio :label="true">是</el-radio>
+            <el-radio :label="false">否</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="排序">
+          <el-input-number v-model="form.orderNum" :min="1" label="描述文件"></el-input-number>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">立即创建</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'MenuCreate',
+  data () {
+    return {
+      form: {
+        parentId: -1,
+        name: '123',
+        href: '123',
+        icon: '123',
+        orderNum: 0,
+        description: '123',
+        shown: false
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
